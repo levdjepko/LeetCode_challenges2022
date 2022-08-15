f4c3018a0427b63f44c591b0c318825111d45581
Lev <levdjepko@gmail.com>
2022-08-14 18:54:36 -0700

    Create arraysContainsDuplicate.java

---
+++ arraysContainsDuplicate.java
@@ -0,0 +1,8 @@
+int[] nums = {1,2,3,4,5,6};
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
