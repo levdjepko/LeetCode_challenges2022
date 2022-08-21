e3aaf57abf2ed775121960cdfb804f88d0790cd7
Lev <levdjepko@gmail.com>
2022-08-20 21:10:50 -0700

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -1,10 +1,12 @@
 public int[] twoSum(int[] nums, int target) {
         int [] results = {0,0};
+        boolean foundResult = false;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if ((nums[i] + nums[j]) == target) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target && !foundResult) {
                     results[0] = i;
                     results[1] = j;
+                    foundResult = true;
                     break;
                 }
             }
