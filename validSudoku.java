46d12aeb1a3c55233effead493fb49519cc0fa76
Lev <levdjepko@gmail.com>
2022-08-22 20:41:52 -0700

    Update validSudoku.java
    work in progress

--- validSudoku.java
+++ validSudoku.java
@@ -2,8 +2,20 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         // check if Sudoku board is valid
         // check rows:
-        for (int i = 0; i < board.length; i++) {
-            
+        int[] check = new int[20];
+        Arrays.fill(check, 0);
+        boolean validBoard = true;
+        
+        for (int i = 0; i < board.length && validBoard; i++) {
+            Arrays.fill(check, 0);
+            for (int column = 0; column < board[i].length && validBoard; column++) {
+                if (Character.isDigit(board[i][column])){
+                    check[(int)board[i][column]]++;
+                }
+                if (check[(int)board[i][column]] > 1) {
+                    validBoard = false;
+                }
+            }
         }
-    }
+        return validBoard;
 }
