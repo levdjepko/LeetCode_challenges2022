3ea0f8fa2f7f3a0e867131bbadd9de7da7350863
Lev <levdjepko@gmail.com>
2022-08-22 20:33:31 -0700

    Create validSudoku.java

---
+++ validSudoku.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // check if Sudoku board is valid
+        // check rows:
+        for (int i = 0; i < board.length; i++) {
+            
+        }
+    }
+}
