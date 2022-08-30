3d445eb5968d36107b0acb41767289acd9c73cab
Lev <levdjepko@gmail.com>
2022-08-29 20:17:20 -0700

    Create rotate2DarrayInPlace.java
    TODO: work in progress

---
+++ rotate2DarrayInPlace.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        /* Input: matrix =  [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+                    Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]] */
+        // Looks like there is a trick to rotate the matrix if we look at the above examples
+        // Let's try to rotate it in another matrix first
+        for (int i = 0; i < matrix.length; i++) {
+            // iterate over rows
+            
+        }
+    }
+}
