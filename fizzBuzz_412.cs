deb738a61c892062aa24093ad7d8d1887e1df90e
Lev <levdjepko@gmail.com>
2023-03-24 08:54:02 -0700

    Create fizzBuzz_412.cs

---
+++ fizzBuzz_412.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        IList<string> result = new List<string>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    result.Add("FizzBuzz");
+                }
+                else {
+                    result.Add("Fizz");
+                }
+            }
+            else if (i % 5 == 0) {
+                result.Add("Buzz");
+            }    
+            else {
+                result.Add(i.ToString());
+            }
+        }
+        return result;
+    }
+}
+//right in the middle
