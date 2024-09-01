f78798cd87bbebb213e57dc8131bc2b04974dc83
Lev <levdjepko@gmail.com>
2024-08-31 21:03:03 -0600

    Update numberOfStepsToZero_1343.cs

--- numberOfStepsToZero_1343.cs
+++ numberOfStepsToZero_1343.cs
@@ -1,4 +1,5 @@
 public int NumberOfSteps(int num) {
+        // weird task from the leet code
         int steps = 0;
         while (num > 0) {
             if (num % 2 == 0) { // even -> divide by two
