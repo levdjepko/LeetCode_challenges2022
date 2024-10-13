ecda3fc0d51ff00b0a179deb26643720e47daa55
Lev <levdjepko@gmail.com>
2024-10-12 17:28:47 -0700

    Update firstUniqueCharacterInString.cs

--- firstUniqueCharacterInString.cs
+++ firstUniqueCharacterInString.cs
@@ -8,11 +8,11 @@ public class Solution {
             hs.Add(s[i], i); // now we have a dictionary item like this: --- unique char --- its index ---
         } else
         {
-            hs[s[i]] = -1; // here we replace the real index with -1 as this can't be an answer
+            hs[s[i]] = -1; // Here we replace the real index with -1 as this can't be an answer
         }
     }
     // iterate over the dictionary again and find the first non -1 item
-    foreach(var item in hs.Keys)
+    foreach (var item in hs.Keys)
     {
         var value = hs[item];
         if (value != -1)
