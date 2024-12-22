e2724c3443d923308662e725420c5a04f1cc9206
Lev <levdjepko@gmail.com>
2024-12-22 08:45:29 -0800

    Update strStr.py

--- strStr.py
+++ strStr.py
@@ -1,4 +1,4 @@
-#Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# Given two strings needle and a haystack, return the index of the first occurrence of a needle in a haystack, or -1 if the needle is not part of the haystack.
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
