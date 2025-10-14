5f31bca2cd5e82b3a9d403a4253ffaa6fe7f0abb
Lev <levdjepko@gmail.com>
2025-10-14 09:21:00 -0700

    Update reverseString.py

--- reverseString.py
+++ reverseString.py
@@ -3,7 +3,16 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        for i in range (0, len(s)//2):
-            temp = s[i]
-            s[i] = s[len(s) - 1 - i]
-            s[len(s) - 1 - i] = temp
+        # for i in range (0, len(s)//2):
+        #     temp = s[i]
+        #     s[i] = s[len(s) - 1 - i]
+        #     s[len(s) - 1 - i] = temp
+        
+        # OR: using two pointer method instead:
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            s[left], s[right] = s[right],s[left]
+            left += 1
+            right -= 1
+            
